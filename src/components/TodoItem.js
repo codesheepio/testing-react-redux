f3a60f8162f3b6ebb3a8829f8ccb3395d3c18969
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-07 22:47:50 +0700

    Create TodoItem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react'
+
+const TodoInput = props => (
+  <li className={props.complete ? 'complete' : ''}>{props.text}</li>
+)
+
+TodoInput.propTypes = {
+  complete: PropTypes.bool,
+  text: PropTypes.string.isRequired,
+}
+
+export default TodoInput
