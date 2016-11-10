cce6a054d73a6e959faf36c433bcb99f682d1e64
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-11 00:11:34 +0700

    TodosReducer handles ADD_TODO action

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,5 +1,14 @@
+import { ADD_TODO } from '../../src/actions/types'
+
 const todosReducer = (state = [], action) => {
   switch (action.type) {
+    case ADD_TODO: {
+      const nextId = state.reduce((max, todo) => (Math.max(max, todo.id)), 0) + 1
+      return [
+        ...state,
+        { id: nextId, text: action.payload.text, complete: false },
+      ]
+    }
     default:
       return state
   }
--- test/reducers/todoReducer.spec.js
+++ test/reducers/todoReducer.spec.js
@@ -1,5 +1,6 @@
 import { expect } from '../testHelper'
 import todosReducer from '../../src/reducers/todosReducer'
+import { ADD_TODO } from '../../src/actions/types'
 
 describe('todosReducer', () => {
   it('returns correct initial state', () => {
@@ -13,5 +14,24 @@ describe('todosReducer', () => {
     const nextState = todosReducer(curState, {})
     expect(nextState).to.equal(curState)
     expect(nextState).to.deep.equal(curState)
+  })
+
+  it('adds todos when receviing ADD_TODO action', () => {
+    const curState = [
+      { id: 1, text: 'Feed cat', complete: false },
+    ]
+    const action = {
+      type: ADD_TODO,
+      payload: {
+        text: 'Sleep',
+      },
+    }
+    const nextState = todosReducer(curState, action)
+    const expectedState = [
+      { id: 1, text: 'Feed cat', complete: false },
+      { id: 2, text: 'Sleep', complete: false },
+    ]
+    expect(nextState).to.deep.equal(expectedState)
+    expect(nextState).to.not.equal(curState)
   })
 })
